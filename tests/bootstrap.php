9d81856e6955bb1cf8ce9227112c9d9f22add367
johnstevenson <john-stevenson@blueyonder.co.uk>
2016-01-04 21:06:19 +0000

    Update to psr4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,21 @@
 <?php
+/*
+ * This file is part of the Json-Works package.
+ *
+ * (c) John Stevenson <john-stevenson@blueyonder.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-spl_autoload_register(function ($class) {
-    if (false !== strpos($class, 'JsonWorks\\')) {
-        $path = 0 === strpos($class, 'JohnStevenson\\') ? 'src' : 'tests';
-        $classFile = str_replace('\\', '/', $class).'.php';
-        require dirname(__DIR__).'/'.$path.'/'.$classFile;
-    }
-});
+error_reporting(E_ALL);
+$autoloader = dirname(__DIR__) . '/vendor/autoload.php';
+
+if (!file_exists($autoloader)) {
+    echo "Composer autoloader not found: $autoloader" . PHP_EOL;
+    echo "Please run 'composer install' and try again." . PHP_EOL;
+    exit(1);
+}
+
+$loader = require $autoloader;
+$loader->addPsr4('JsonWorks\\Tests\\', __DIR__);
