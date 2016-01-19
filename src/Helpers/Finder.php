1ac88d914a916380b2a101865715870945accb2a
johnstevenson <john-stevenson@blueyonder.co.uk>
2016-01-19 12:46:30 +0000

    Improve and document finder class

--- src/Helpers/Finder.php
+++ src/Helpers/Finder.php
@@ -11,28 +11,31 @@
 namespace JohnStevenson\JsonWorks\Helpers;
 
 /**
-* A class for finding value using JSON Pointers
+* A class for finding a value using JSON Pointers
 */
 class Finder
 {
+    /**
+    * @var mixed
+    */
     protected $element;
 
+    /**
+    * Searches for and returns a reference to an element
+    *
+    * @param mixed $data
+    * @param array $tokens Set to last known token on failure
+    * @param bool $found Set by method
+    * @return mixed A reference to the found element
+    */
     public function &get(&$data, &$tokens, &$found)
     {
         $this->element =& $data;
 
         while (!empty($tokens)) {
-            $found = false;
-            $type = gettype($this->element);
-            $key = $tokens[0];
+            $token = $tokens[0];
 
-            if ('object' === $type) {
-                $found = $this->findObject($key);
-            } elseif ('array' === $type) {
-                $found = $this->findArray($key);
-            }
-
-            if (!$found) {
+            if (!$found = $this->find($token)) {
                 break;
             }
 
@@ -42,9 +45,38 @@ class Finder
         return $this->element;
     }
 
-    protected function findArray($key)
+    /**
+    * Returns true if a token is found at the data root
+    *
+    * A reference to the value is placed in $this->element
+    *
+    * @param string $token
+    * @return bool
+    */
+    protected function find($token)
     {
-        if ($result = $this->isArrayKey($key, $index)) {
+        $found = false;
+        $type = gettype($this->element);
+
+        if ('object' === $type) {
+            $found = $this->findObject($token);
+        } elseif ('array' === $type) {
+            $found = $this->findArray($token);
+        }
+
+        return $found;
+    }
+
+    /**
+    * Returns true if the token is an array key
+    *
+    * Sets $this->element to reference the value
+    * @param string $token
+    * @return bool
+    */
+    protected function findArray($token)
+    {
+        if ($result = $this->isArrayKey($token, $index)) {
             if ($result = array_key_exists($index, $this->element)) {
                 $this->element = &$this->element[$index];
             }
@@ -53,21 +85,34 @@ class Finder
         return $result;
     }
 
-    protected function findObject($key)
+    /**
+    * Returns true if the token is an object property key
+    *
+    * Sets $this->element to reference the value
+    * @param string $token
+    * @return bool
+    */
+    protected function findObject($token)
     {
-        if ($result = property_exists($this->element, $key)) {
-            $this->element = &$this->element->$key;
+        if ($result = property_exists($this->element, $token)) {
+            $this->element = &$this->element->$token;
         }
 
         return $result;
     }
 
-    protected function isArrayKey($value, &$index)
+    /**
+    * Returns true if the token is a valid array key
+    *
+    * @param string $token
+    * @param mixed $index Set to an integer on success
+    */
+    protected function isArrayKey($token, &$index)
     {
         $index = null;
 
-        if (preg_match('/^0*\d+$/', $value)) {
-            $index = (int) $value;
+        if (preg_match('/^0*\d+$/', $token)) {
+            $index = (int) $token;
         }
 
         return $index !== null;
