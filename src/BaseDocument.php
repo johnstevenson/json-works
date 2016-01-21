89097ec8352f902b59563981e553fc6ff1ac3278
johnstevenson <john-stevenson@blueyonder.co.uk>
2016-01-21 15:00:17 +0000

    Split Document class

---
+++ src/BaseDocument.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace JohnStevenson\JsonWorks;
+
+use JohnStevenson\JsonWorks\Helpers\Formatter;
+use JohnStevenson\JsonWorks\Schema\Model;
+use JohnStevenson\JsonWorks\Schema\Validator;
+
+class BaseDocument
+{
+    public $data;
+    public $schema;
+    public $lastError;
+
+    /**
+    * @var Formatter
+    */
+    protected $formatter;
+
+    /**
+    * @var Validator
+    */
+    protected $validator;
+
+    public function __construct()
+    {
+        $this->formatter = new Formatter();
+    }
+
+    public function loadData($data, $noException = false)
+    {
+        $data = $this->getInput($data, false, $noException);
+        $this->data = $data ? $this->formatter->copy($data) : null;
+        return empty($this->lastError);
+    }
+
+    public function loadSchema($schema, $noException = false)
+    {
+        $this->schema = $this->getInput($schema, true, $noException);
+        return empty($this->lastError);
+    }
+
+    public function tidy($order = false)
+    {
+        $this->data = $this->formatter->prune($this->data);
+
+        if ($order && $this->schema) {
+            $this->data = $this->formatter->order($this->data, $this->schema->data);
+        }
+    }
+
+    public function toJson($pretty)
+    {
+        $options = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
+        $options |= $pretty ? JSON_PRETTY_PRINT : 0;
+
+        return json_encode($this->data, $options);
+    }
+
+    public function validate($lax = false)
+    {
+        if (!$this->schema) {
+            return true;
+        }
+
+        if (!$this->validator) {
+            $this->validator = new Validator();
+        }
+
+        if (!$result = $this->validator->check($this->data, $this->schema, $lax)) {
+            $this->lastError = $this->validator->error;
+        }
+
+        return $result;
+    }
+
+    protected function getInput($input, $isSchema, $noException)
+    {
+        $this->lastError = null;
+        $input = $this->getInputWork($input, $isSchema);
+
+        if (false === $input) {
+            $this->lastError = $this->lastError ?: 'Invalid input';
+        }
+
+        if ($this->lastError !== null && !$noException) {
+            throw new \RuntimeException($this->lastError);
+        }
+
+        return $this->lastError ? null : $input;
+    }
+
+    protected function getInputWork($input, $isSchema)
+    {
+        if (is_string($input)) {
+
+            if (!preg_match('/^(\{|\[)/', $input)) {
+                $filename = $input;
+
+                if (!$input = @file_get_contents($filename)) {
+                    if (false === $input) {
+                        $this->lastError = 'Unable to open file: '.$filename;
+                    } else {
+                        $this->lastError = 'File is empty: '.$filename;
+                    }
+
+                    return false;
+                }
+            }
+
+            $input = json_decode($input);
+            if (json_last_error()) {
+                return false;
+            }
+        }
+
+        if (is_array($input) || is_null($input)) {
+            $result = !$isSchema;
+        } else {
+            $result = is_object($input);
+        }
+
+        if ($result && $isSchema) {
+            try {
+                $input = new Model($input);
+            } catch (\RuntimeException $e) {
+                $result = false;
+                $this->lastError = 'Schema error: '.$e->getMessage();
+            }
+        }
+
+        return $result ? $input : false;
+    }
+}
--- src/Document.php
+++ src/Document.php
@@ -4,15 +4,10 @@ namespace JohnStevenson\JsonWorks;
 
 use JohnStevenson\JsonWorks\Helpers\Builder;
 use JohnStevenson\JsonWorks\Helpers\Finder;
-use JohnStevenson\JsonWorks\Helpers\Formatter;
 use JohnStevenson\JsonWorks\Helpers\Tokenizer;
 
-class Document
+class Document extends BaseDocument
 {
-    public $data;
-    public $schema;
-    public $lastError;
-
     /**
     * @var Builder
     */
@@ -24,36 +19,17 @@ class Document
     protected $finder;
 
     /**
-    * @var Formatter
-    */
-    protected $formatter;
-
-    /**
     * @var Tokenizer
     */
     protected $tokenizer;
 
-    private $validator;
-
     public function __construct()
     {
+        parent::__construct();
+
         $this->builder = new Builder();
         $this->finder = new Finder();
-        $this->formatter = new Formatter();
         $this->tokenizer = new Tokenizer();
-    }
-
-    public function loadData($data, $noException = false)
-    {
-        $data = $this->getInput($data, false, $noException);
-        $this->data = $data ? $this->formatter->copy($data) : null;
-        return empty($this->lastError);
-    }
-
-    public function loadSchema($schema, $noException = false)
-    {
-        $this->schema = $this->getInput($schema, true, $noException);
-        return empty($this->lastError);
     }
 
     public function addValue($path, $value)
@@ -120,86 +96,6 @@ class Document
         return $this->workMove($fromPath, $toPath, true);
     }
 
-    public function tidy($order = false)
-    {
-        $this->data = $this->formatter->prune($this->data);
-
-        if ($order && $this->schema) {
-            $this->data = $this->formatter->order($this->data, $this->schema->data);
-        }
-    }
-
-    public function toJson($pretty)
-    {
-        $options = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
-        $options |= $pretty ? JSON_PRETTY_PRINT : 0;
-
-        return json_encode($this->data, $options);
-    }
-
-    public function validate($lax = false)
-    {
-        return $this->checkData($this->data, $lax);
-    }
-
-    protected function getInput($input, $isSchema, $noException)
-    {
-        $this->lastError = null;
-        $input = $this->getInputWork($input, $isSchema);
-
-        if (false === $input) {
-            $this->lastError = $this->lastError ?: 'Invalid input';
-        }
-
-        if ($this->lastError !== null && !$noException) {
-            throw new \RuntimeException($this->lastError);
-        }
-
-        return $this->lastError ? null : $input;
-    }
-
-    protected function getInputWork($input, $isSchema)
-    {
-        if (is_string($input)) {
-
-            if (!preg_match('/^(\{|\[)/', $input)) {
-                $filename = $input;
-
-                if (!$input = @file_get_contents($filename)) {
-                    if (false === $input) {
-                        $this->lastError = 'Unable to open file: '.$filename;
-                    } else {
-                        $this->lastError = 'File is empty: '.$filename;
-                    }
-
-                    return false;
-                }
-            }
-
-            $input = json_decode($input);
-            if (json_last_error()) {
-                return false;
-            }
-        }
-
-        if (is_array($input) || is_null($input)) {
-            $result = !$isSchema;
-        } else {
-            $result = is_object($input);
-        }
-
-        if ($result && $isSchema) {
-            try {
-                $input = new Schema\Model($input);
-            } catch (\RuntimeException $e) {
-                $result = false;
-                $this->lastError = 'Schema error: '.$e->getMessage();
-            }
-        }
-
-        return $result ? $input : false;
-    }
-
     protected function workMove($fromPath, $toPath, $delete)
     {
         $result = false;
@@ -210,23 +106,6 @@ class Document
                     $this->deleteValue($fromPath);
                 }
             }
-        }
-
-        return $result;
-    }
-
-    protected function checkData($data, $lax = false)
-    {
-        if (!$this->schema) {
-            return true;
-        }
-
-        if (!$this->validator) {
-            $this->validator = new Schema\Validator();
-        }
-
-        if (!$result = $this->validator->check($data, $this->schema, $lax)) {
-            $this->lastError = $this->validator->error;
         }
 
         return $result;
