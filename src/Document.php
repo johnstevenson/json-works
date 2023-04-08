452dd9ae1e9608cb5e8f867cca0cc0e8e232c31a
John Stevenson <john-stevenson@blueyonder.co.uk>
2023-04-08 11:04:48 +0100

    Add and update license details

--- src/Document.php
+++ src/Document.php
@@ -1,5 +1,14 @@
 <?php declare(strict_types=1);
 
+/*
+ * This file is part of the Json-Works package.
+ *
+ * (c) John Stevenson <john-stevenson@blueyonder.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace JohnStevenson\JsonWorks;
 
 use JohnStevenson\JsonWorks\Helpers\Patcher;
